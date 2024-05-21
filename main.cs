df1dc19ebc2e2e2d797e519782ba3919c2909a68
Ka2sperr <75748370+Ka2sperr@users.noreply.github.com>
2024-05-21 22:31:32 +0200

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BasicAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            int sum = number1 + number2;
+
+            Console.WriteLine("The sum of {0} and {1} is: {2}", number1, number2, sum);
+        }
+    }
+}
